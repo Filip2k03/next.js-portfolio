97937d52d0c2441fc6ed2fa339e2973b755c3a32
StephanFilip2k03 <stephanfilip7@gmail.com>
2025-02-11 15:59:07 +0000

    finish contact us

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { FaLinkedin, FaGithub, FaFacebook, FaInstagram } from 'react-icons/fa';
+import styles from '../styles/Contact.module.css';
 
 const Contact = () => {
     return (
         <div>
             <Header />
-            <section className="home">
-                <div className="home-content">
+            <section className={styles.contactSection}>
+                <div className={styles.contactContent}>
                     <h1>Contact<span> Us</span></h1>
-                    <h3 className="typing-text-1">For <span></span></h3>
-                    <div className="social-icons">
-                        <a href="#"><i className="fa-brands fa-linkedin"></i></a>
-                        <a href="#"><i className="fa-brands fa-github"></i></a>
-                        <a href="#"><i className="fa-brands fa-x-twitter"></i></a>
-                        <a href="#"><i className="fa-brands fa-instagram"></i></a>
+                    <h3 className={styles.typingText}>For <span>Inquiries</span></h3>
+                    <div className={styles.socialIcons}>
+                        <a href="https://www.linkedin.com/in/thu-ya-kyaw-5a606732b"><FaLinkedin /></a>
+                        <a href="https://github.com/Filip2k03"><FaGithub /></a>
+                        <a href="#"><FaFacebook /></a>
+                        <a href="#"><FaInstagram /></a>
                     </div>
                 </div>
 
-                <div className="contact-form">
+                <div className={styles.contactForm}>
                     <form action="mailto:stephanfilip7@gmail.com" method="post" enctype="text/plain">
                         <label htmlFor="name">Name:</label>
                         <input type="text" id="name" name="name" required />
@@ -29,13 +31,13 @@ const Contact = () => {
                         <label htmlFor="message">Message:</label>
                         <textarea id="message" name="message" rows="4" required></textarea>
                         
-                        <button type="submit" className="btn">Submit</button>
+                        <button type="submit" className={styles.btn}>Submit</button>
                     </form>
                 </div>
 
-                <div className="contact-buttons">
-                    <a href="tel:+959954480806" className="btn"><i className="fa fa-phone"></i> Call Us</a>
-                    <a href="mailto:stephanfilip7@gmail.com" className="btn"><i className="fa fa-envelope"></i> Email Us</a>
+                <div className={styles.contactButtons}>
+                    <a href="tel:+959954480806" className={styles.btn}><i className="fa fa-phone"></i> Call Us</a>
+                    <a href="mailto:stephanfilip7@gmail.com" className={styles.btn}><i className="fa fa-envelope"></i> Email Us</a>
                 </div>
             </section>
             <Footer />
